5113e5aaa385280d2b59d2c2ff351330e59c6f22
himabindu08 <42699277+himabindu08@users.noreply.github.com>
2018-09-01 20:36:11 -0500

    rock

--- rockpaper/game.js
+++ rockpaper/game.js
@@ -1,11 +1,13 @@
-function game() {
-    var number = prompt("Please enter your number");
-    var random = Math.floor((Math.random() * 5) + 1);
-    if (number == random) {
-        document.getElementById('OUTPUT').innerHTML = 'WIN!!!!!!';
+function game() //creating a function named game
+{
+    var number = prompt("Please enter your number"); // entering a number by user
+    var random = Math.floor((Math.random() * 5) + 1); // providing random number range from 1 to 5
+    if (number == random) // if condition to check whether the entered number matches the random number
+    {
+        document.getElementById('OUTPUT').innerHTML = 'WIN!!!!!!'; // providing output as WIN if true
     }
     else {
-       game();
+       game(); //repeating the loop if false,by calling the function
     }
 }
 game();
